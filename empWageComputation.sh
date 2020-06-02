110831efa35e17658e3b26837b76538eb92d4547
Ch Deepthi <chandrapatideepthi@gmail.com>
2020-06-02 14:42:54 +0530

    Added: Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -x
 echo "Welcome to EmpWage program"
-isPresent=1;
-randomCheck=$(( $RANDOM %2 ))
-Salary=0;
-if [ $isPresent -eq $randomCheck ];
-        then
-		echo "Employee is Present"
-else 
-	echo "Employee is absent"
-fi
+isFullTime=1;
+empRatePerHr=20;
+empCheck=$((RANDOM%2));
+case $empCheck in
+	$isFullTime)
+		empHrs=8
+		;;
+	*)
+	empHrs=0
+		;;
+esac
+salary=$(($empHrs+$empRatePerHr));
+
+
 
