04bed6651c7207fad4f7901747a0c273965a0626
Ch Deepthi <chandrapatideepthi@gmail.com>
2020-06-02 15:50:35 +0530

    Added: Wages for a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,8 +2,13 @@
 echo "Welcome to EmpWage program"
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
 empRatePerHr=20;
-empCheck=$(( RANDOM % 3 ));
+numWorkingDays=20;
+empCheck= $(( RANDOM % 3 ));
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	empCheck=$(( RANDOM % 3 ));
 case $empCheck in
 	$isFullTime)
 		empHrs=8
@@ -12,10 +17,13 @@ case $empCheck in
 		empHrs=8
 		;;
 	*)
-	empHrs=0
+		empHrs=0
 		;;
 esac
 salary=$(($empHrs+$empRatePerHr));
+totalSalary=$(( $totalSalary+$salary ));
+done
+
 
 
 
