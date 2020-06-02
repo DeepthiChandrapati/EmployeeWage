d34fc08746d03b2b2724e42f44afb6f2fb593498
Ch Deepthi <chandrapatideepthi@gmail.com>
2020-06-02 14:26:11 +0530

    Added: Checking Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,12 @@
+#!/bin/bash -x
 echo "Welcome to EmpWage program"
+isPresent=1;
+randomCheck=$(( $RANDOM %2 ))
+Salary=0;
+if [ $isPresent -eq $randomCheck ];
+        then
+		echo "Employee is Present"
+else 
+	echo "Employee is absent"
+fi
+
